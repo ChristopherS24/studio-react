3d793510287785823119056d13121e346108ab99
ChristopherS24 <chryskies@gmail.com>
2025-02-05 19:16:19 +0100

    aggiunta navbar di prova

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar(){
+    return (
+        <ul>
+            <li>
+                <a href="#">Hola</a>
+            </li>
+            <li>
+                <a href="#">Hola</a>
+            </li>
+            <li>
+                <a href="#">Hola</a>
+            </li>
+            <li>
+                <a href="#">Hola</a>
+            </li>
+        </ul>
+    );
+}
+
+export default Navbar
