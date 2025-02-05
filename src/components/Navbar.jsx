3ae001dc92dfd54eb7153bb49fbe2177a45fd6c9
ChristopherS24 <chryskies@gmail.com>
2025-02-06 00:58:47 +0100

    prova primi stili

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,27 @@ import './Navbar.css';
 import Link from './Link';
 
 function Navbar(){
+
+    const x = 11;
+
     return (
-        <ul>
-            <li>
-                <Link>Hola</Link>
-            </li>
-            <li>
-                <Link>Chi siamo</Link>
-            </li>
-            <li>
-                <Link>Contatti</Link>
-            </li>
-            <li>
-                <Link>Dove trovarci</Link>
-            </li>
-        </ul>
+        <>
+        <div className={`box rounded ${x < 10 ? "rotated" : ""}`}>x è {x}</div>
+            <ul>
+                <li>
+                    <Link>Hola</Link>
+                </li>
+                <li>
+                    <Link>Chi siamo</Link>
+                </li>
+                <li>
+                    <Link>Contatti</Link>
+                </li>
+                <li>
+                    <Link>Dove trovarci</Link>
+                </li>
+            </ul>
+        </>
     );
 }
 
