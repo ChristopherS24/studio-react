cbe6c9023f4d2e3257b084ecbacbdb9b670a3382
ChristopherS24 <chryskies@gmail.com>
2025-02-06 01:50:16 +0100

    installato tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import Link from './Link';
 function Navbar(){
 
     const x = 11;
+    const y = 1;
 
     return (
         <>
         <div className={`box rounded ${x < 10 ? "rotated" : ""}`}>x è {x}</div>
+        <div className={`bg-blue-500 rounded-lg w-72 h-72  ${y < 10 ? "rotate-45" : "bg-yellow-300"}`}>hola</div>
             <ul>
                 <li>
                     <Link>Hola</Link>
